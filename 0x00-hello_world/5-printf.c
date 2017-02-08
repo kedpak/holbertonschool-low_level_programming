f4a947f15f252a08b6f3a5cc661ba557206ccbd3
kedpak <kedpak@yahoo.com>
2017-02-08 21:55:28 +0000

    added comments to describe function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ * main - main function that prints a string
+ * Return - returns a string
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
