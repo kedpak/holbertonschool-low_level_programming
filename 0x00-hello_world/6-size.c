b3bea7d807699b05d83beb121d7adfb39d25638c
kedpak <kedpak@yahoo.com>
2017-02-08 20:53:13 +0000

    data type printed size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
 	char charSize;
 	int intSize;
 	long longSize;
 	double doubleSize;
 	float floatSize;
-	
+
 	printf("Size of a char: %d byte(s)\n", sizeof(charSize));
 	printf("Size of an int: %d byte(s)\n", sizeof(intSize));
 	printf("Size of a long int: %d byte(s)\n", sizeof(longSize));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(doubleSize));
 	printf("Size of a float: %d byte(s)\n", sizeof(floatSize));
-	
-	return 0;
+	return (0);
 }
