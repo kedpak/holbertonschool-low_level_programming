3841b4db54c2619005e5c67969034beee2dc6fc2
kedpak <kedpak@yahoo.com>
2017-02-08 21:52:49 +0000

    made comment to describe function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - this is the main function
+ * Return: a string
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
