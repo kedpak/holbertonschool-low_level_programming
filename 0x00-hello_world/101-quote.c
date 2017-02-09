75cf9641e86b067f002255ffbf4e694772c2c286
kedpak <kedpak@yahoo.com>
2017-02-09 04:05:21 +0000

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,10 +6,6 @@
  */
 int main(void)
 {
-	int i;
-	char c[60] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	for(i = 0; i < 61; i++) {
-		putchar(c[i]);
-	}
-      	return (1);
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
