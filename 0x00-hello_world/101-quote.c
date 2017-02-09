87e58a131fa105eaf2f608f94802a83f8d3e9be7
kedpak <kedpak@yahoo.com>
2017-02-09 06:03:54 +0000

    write functioin

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
-
+#include <unistd.h>
+#include <sys/types.h>
 /**
  * main -prints string
  * Return: a string
  */
+
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	
 	return (1);
+
 }
+
