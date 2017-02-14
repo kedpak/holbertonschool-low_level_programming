1934d44cda22820f4cfd0a8891e6c0366b9b2eba
kedpak <kedpak@yahoo.com>
2017-02-14 03:05:09 +0000

    first commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _islower - returns 1 if character is lowercase, 0 if not
+ * Return: 1 if lowercase, 0 if not
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
