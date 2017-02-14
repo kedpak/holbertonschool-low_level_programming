5c0a677543fd743f25d500f10783aaa22081e71c
kedpak <kedpak@yahoo.com>
2017-02-14 02:07:32 +0000

    first commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - prints characters a-z 
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+        int i = 0;
+        char alpha = 'a';
+
+        while(i < 26)
+        {
+		i++;
+                _putchar(alpha);
+                alpha++;
+        }
+	_putchar('\n');
+}
