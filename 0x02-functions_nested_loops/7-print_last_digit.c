7820aa7570747dcf0cfab4f02b2b14b57f43d3b4
kedpak <kedpak@yahoo.com>
2017-02-14 09:45:54 +0000

    first commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - prints last digit
+ * Return: remainder
+ */
+int print_last_digit(int n)
+{
+	int l = n % 10;
+
+	if (n > 0)
+	{
+		_putchar(l + '0');
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+	}
+	else if (n < 0)
+	{
+		l *= -1;
+		_putchar(l + '0');
+	}
+	return (l);
+}
