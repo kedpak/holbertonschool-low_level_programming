a94e337698ade449f098138e4ccda3ff58199ffc
kedpak <kedpak@yahoo.com>
2017-02-14 20:10:00 +0000

    fixed betty issues

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * _islower - returns 1 if character is lowercase, 0 if not
+ * _isalpha - returns 1 if character is alphabet, 0 if not
  * @c: integer input
  * Return: 1 if lowercase, 0 if not
  */
