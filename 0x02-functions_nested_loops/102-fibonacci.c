d91d2eaa9afda2c8d95e35a329334a5282fbf0fa
kedpak <kedpak@yahoo.com>
2017-02-16 02:30:31 +0000

    first commit

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints first 50 fibonacci numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int i, n, k;
+
+	n = 1;
+	k = 2;
+	for (i = 0; i <= 50; i++)
+	{
+		printf("%d ,", n);
+		printf("%d ,", k);
+		n = n + k;
+		k = n + k;
+	}
+	putchar('\n');
+	return (0);
+}
