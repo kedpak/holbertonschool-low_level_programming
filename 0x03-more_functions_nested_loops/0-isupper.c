114ebc56c9a20d52292746477885925cb31baadc
kedpak <kedpak@yahoo.com>
2017-02-16 08:26:22 +0000

    first commit

---
+++ 0x03-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _isupper - returns 1 if upper case 0 if lower case
+ * @c: integer of characeter argumetn
+ * Return: 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else if (c >= 'a' && c <= 'z')
+	{
+		return (0);
+	}
+	return (c);
+}
