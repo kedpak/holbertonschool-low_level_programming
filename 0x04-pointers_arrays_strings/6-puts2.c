bee9bbc7233a6646c3356a10b6c7e61cefbf72ea
kedpak <kedpak@yahoo.com>
2017-02-21 18:13:07 +0000

    first commit

---
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * puts2 - prints every other character in string
+ * @str: string input
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
