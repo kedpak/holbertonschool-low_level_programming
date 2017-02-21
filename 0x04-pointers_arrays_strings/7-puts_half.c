a2f28923711b36cbe7296f077b406cfeeb3b35e1
kedpak <kedpak@yahoo.com>
2017-02-21 22:17:49 +0000

    fixed character issue

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -7,24 +7,29 @@
  */
 void puts_half(char *str)
 {
-	int length, i;
+	int length, i, n;
 
+	length = 0;
+	n = 0;
 	while (str[length] != '\0')
 	{
 		length++;
 	}
+
 	if (length % 2 == 0)
 	{
 		for (i = 0; i < length / 2; i++)
 		{
-			_putchar(*(str + (length / 2)) + i);
+			_putchar(*(str + (length / 2) + n));
+			n++;
 		}
 	}
 	else if (length % 2 == 1)
 	{
 		for (i = 0; i < length / 2; i++)
 		{
-			_putchar(*(str + ((length - 1) / 2)) + i);
+			_putchar(*(str + ((length - 1) / 2) + n));
+			n++;
 		}
 	}
 
