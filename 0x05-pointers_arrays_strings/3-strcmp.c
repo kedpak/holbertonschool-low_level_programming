50791a2e1fe68935cd955909a048bccf0015b197
kedpak <kedpak@yahoo.com>
2017-02-23 23:49:54 +0000

    added break in correct spot

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -17,15 +17,16 @@ int _strcmp(char *s1, char *s2)
 		if (*s1 == *s2)
 		{
 			num = 0;
-			break;
 		}
 		else if (*s1 > *s2)
 		{
 			num = *s1 - *s2;
+			break;
 		}
 		else if (*s1 < *s2)
 		{
 			num = *s1 - *s2;
+			break;
 		}
 		i++;
 		j++;
