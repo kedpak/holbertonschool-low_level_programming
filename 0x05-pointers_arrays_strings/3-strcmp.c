961a2a1e67e4f36803f7b4e4890a81d3f911f7ac
kedpak <kedpak@yahoo.com>
2017-02-23 23:36:53 +0000

    added some fixes to loop

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -8,24 +8,26 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int num, i;
+	int num, i, j;
 	
 	i = 0;
-	while (s1[i] != '\0')
+	j = 0;
+	while (s1[i] != '\0' || s2[j] != '\0')
 	{ 
-		if (*s1 < *s2)
+		if (*s1 == *s2)
 		{
-			num = *s1 - *s2;
+			num = 0;
 		}
 		else if (*s1 > *s2)
 		{
 			num = *s1 - *s2;
 		}
-		else
+		else if (*s1 < *s2)
 		{
-			num = 0;
+			num = *s1 - *s2;
 		}
 		i++;
+		j++;
 	}
 	return (num);
 }
