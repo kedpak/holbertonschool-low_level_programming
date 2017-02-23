d1676e0ac1f2eb27b79490c043859202cb43699f
kedpak <kedpak@yahoo.com>
2017-02-23 23:20:25 +0000

    added loop

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -8,19 +8,24 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int num;
-
-	if (*s1 < *s2)
-	{
-		num = *s1 - *s2;
-	}
-	else if (*s1 > *s2)
-	{
-		num = *s1 - *s2;
-	}
-	else if (*s1 == *s2)
-	{
-		num = *s1 - *s2;;
+	int num, i;
+	
+	i = 0;
+	while (s1[i] != '\0')
+	{ 
+		if (*s1 < *s2)
+		{
+			num = *s1 - *s2;
+		}
+		else if (*s1 > *s2)
+		{
+			num = *s1 - *s2;
+		}
+		else
+		{
+			num = 0;
+		}
+		i++;
 	}
 	return (num);
 }
