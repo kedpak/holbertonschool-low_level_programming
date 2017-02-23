3ed0117156657745debac927bdaaeae57c903ac2
kedpak <kedpak@yahoo.com>
2017-02-23 23:11:41 +0000

    first commit

---
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - compare two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: integer difference of string
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int num;
+
+	if (*s1 < *s2)
+	{
+		num = *s1 - *s2;
+	}
+	else if (*s1 > *s2)
+	{
+		num = *s1 - *s2;
+	}
+	else
+	{
+		num = 0;
+	}
+	return (num);
+}
