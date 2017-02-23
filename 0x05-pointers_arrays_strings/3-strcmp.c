ba529ef5b52cce95ed3f049ab3b8c523dbfc74fc
kedpak <kedpak@yahoo.com>
2017-02-23 23:55:55 +0000

    fixed pointer issue

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -12,18 +12,18 @@ int _strcmp(char *s1, char *s2)
 	
 	i = 0;
 	j = 0;
-	while (s1[i] != '\0' && s2[j] != '\0')
+	while (s1[i] != '\0' || s2[j] != '\0')
 	{ 
-		if (*s1 == *s2)
+		if (s1[i] == s2[j])
 		{
 			num = 0;
 		}
-		else if (*s1 > *s2)
+		else if (s1[i] > s2[j])
 		{
 			num = *s1 - *s2;
 			break;
 		}
-		else if (*s1 < *s2)
+		else if (s1[i] < s2[j])
 		{
 			num = *s1 - *s2;
 			break;
