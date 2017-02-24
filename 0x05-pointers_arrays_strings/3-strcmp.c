3ce18e4a9c88285ed92407fe29f03094d6859e77
kedpak <kedpak@yahoo.com>
2017-02-24 05:37:31 +0000

    changed return

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -8,22 +8,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int num, i;
+	int i;
 
-	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+	for (i = 0; s1[i] != '\0' || s1[i] != '\0'; i++)
 	{
-		if (*(s1 + i) > *(s2 + i) || *(s1 + i) < *(s2 + i))
-		{
-			num = *s1 - *s2;
-			return (num);
-		}
-		else
-		{
-			num = *s1 - *s2;
-		}
-		i++;
+		if (*(s1 + i) != *(s2 + i)
+			{
+			return (s1[i] - s2[i]);
+			}
 	}
 	return (0);
-
 }
