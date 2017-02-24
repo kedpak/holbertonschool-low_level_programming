3a9769946ddf2b75d05e39704f83b168cb531ef4
kedpak <kedpak@yahoo.com>
2017-02-24 20:06:38 +0000

    switched letters

--- 0x05-pointers_arrays_strings/8-rot13.c
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -11,11 +11,11 @@ char *rot13(char *ch)
 
 	for (i = 0; ch[i] != '\0'; i++)
 	{
-		if ((ch[i] >= 'a' && ch[i] <= 'm') || (ch[i] >= 'A' && ch[i] <= 'M'))
+		if ((ch[i] >= 'A' && ch[i] <= 'M') || (ch[i] >= 'a' && ch[i] <= 'm'))
 		{
 			ch[i] += 13;
 		}
-		else if ((ch[i] >= 'n' && ch[i] <= 'z') || (ch[i] >= 'N' && ch[i] <= 'Z'))
+		else if ((ch[i] >= 'N' && ch[i] <= 'Z') || (ch[i] >= 'n' && ch[i] <= 'z'))
 		{
 			ch[i] -= 13;
 		}
