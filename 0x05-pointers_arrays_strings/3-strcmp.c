fc6c3df687bc0801bcdeba5cd5fe267380bdcb13
kedpak <kedpak@yahoo.com>
2017-02-24 02:37:36 +0000

    reduced content

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -9,27 +9,23 @@
 int _strcmp(char *s1, char *s2)
 {
 	int num, i, j;
-	
+
 	i = 0;
 	j = 0;
 	while (s1[i] != '\0' && s2[j] != '\0')
-	{ 
-		if (*(s1 + i) == *(s2 + j))
+	{
+		if (*(s1 + i) > *(s2 + j) || *(s1 + i) < *(s2 + j))
 		{
 			num = *s1 - *s2;
+			return (num);
 		}
-		else if (*(s1 + i) > *(s2 + j))
+		else
 		{
 			num = *s1 - *s2;
-			break;
-		}
-		else if (*(s1 + i) < *(s2 + j))
-		{
-			num = *s1 - *s2;
-			break;
 		}
 		i++;
 		j++;
 	}
 	return (num);
+
 }
