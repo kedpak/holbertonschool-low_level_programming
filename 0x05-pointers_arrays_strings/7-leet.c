00616763e5d4e7f95143e128ad0ef9bf26f0624c
kedpak <kedpak@yahoo.com>
2017-02-24 19:54:12 +0000

    reduced

--- 0x05-pointers_arrays_strings/7-leet.c
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -9,20 +9,16 @@ char *leet(char *s)
 {
 	int i, j;
 	char lower[5] = {'a', 'e', 'o', 't', 'l'};
-	char numbers[5] = {'4', '3', '0', '7', '1'};
-	char *u;
-	char *l;
+	char numbers[5] = {'4', '3', '0', '7', '1'};	
 
-	u = numbers;
-	l = lower;
 	j = 0;
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		while (j < 5)
 		{
-			if (*(s + i) == l[j] || *(s + i) == l[j] - 32)
+			if (*(s + i) == lower[j] || *(s + i) == lower[j] - 32)
 			{
-				*(s + i) = u[j];
+				*(s + i) = numbers[j];
 			}
 			j++;
 		}
