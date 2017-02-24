d0b8789b894fb93b2da24996b8e5845e7f760e36
kedpak <kedpak@yahoo.com>
2017-02-24 04:18:12 +0000

    fixed issue

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -12,12 +12,19 @@ char *cap_string(char *str)
 	i = 0;
 	while (str[i] != '\0')
 	{
+
 		if (*(str + i) >= 97 && *(str + i) <= 122 &&
-*((str + i) - 1) <= 47 && *((str + i) - 1) >= 0)
+*((str + i) - 1) <= 126 && *((str + i) - 1) >= 123)
 		{
 			*(str + i) = *(str + i) - 32;
 
 		}
+		else if (*(str + i) >= 97 && *(str + i) <= 122 &&
+*((str + i) - 1) <= 47 && *((str + i) - 1) >= 0)
+		{
+			*(str + i) = *(str + i) - 32;
+		}
+
 		i++;
 	}
 	return (str);
