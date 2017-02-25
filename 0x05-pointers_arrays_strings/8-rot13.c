16890307406e1566c7f99613ce9485a2f4e62436
kedpak <kedpak@yahoo.com>
2017-02-25 04:15:54 +0000

    no brackets

--- 0x05-pointers_arrays_strings/8-rot13.c
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -12,13 +12,9 @@ char *rot13(char *s)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if ((s[i] >= 'A' && s[i] <= 'M') || (s[i] >= 'a' && s[i] <= 'm'))
-		{
 			s[i] += 13;
-		}
 		else if ((s[i] >= 'N' && s[i] <= 'Z') || (s[i] >= 'n' && s[i] <= 'z'))
-		{
 			s[i] -= 13;
-		}
 	}
 	return (s);
 }
