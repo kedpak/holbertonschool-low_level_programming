520c61da7c8c6572e96b40571043c8f3843b5569
kedpak <kedpak@yahoo.com>
2017-02-27 10:26:35 +0000

    first commit

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _strchr - find character inside string
+ * @s: string to find character
+ * @c: character to find
+ * Return: string
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			s = s + i;
+			return (s);
+		}
+	}
+	return (0);
+}
