2f92a3407267221156332f739c61470dd636c51e
kedpak <kedpak@yahoo.com>
2017-02-27 22:20:05 +0000

    included null character in string search

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -10,11 +10,11 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; *s ; i++)
 	{
 		if (s[i] == c)
 		{
-			s = (s + i) + '\0';
+			s = s + i;
 			return (s);
 		}
 	}
