20b5e4c1b5e2827e2ad02261dd352eb1c35d4612
kedpak <kedpak@yahoo.com>
2017-02-28 23:02:24 +0000

    fixed betty issues

--- 0x06-pointers_arrays_strings/8-print_diagsums.c
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -1,12 +1,18 @@
 #include "holberton.h"
 #include <stdio.h>
 
+/**
+ * print_diagsums - print sum of arrays diagonal
+ * @a: array pointer
+ * @size: size of array
+ * Return: sum
+ */
 void print_diagsums(int *a, int size)
 {
 	int i, sum, sum2, n;
 
 	n = 0;
-	sum = 0;;
+	sum = 0;
 	sum2 = 0;
 	for (i = 0; i < size; i++)
 	{
