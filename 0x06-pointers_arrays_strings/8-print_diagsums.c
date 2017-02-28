efad186ed2d63ef26053c07abb723482ce5669e8
kedpak <kedpak@yahoo.com>
2017-02-28 22:12:56 +0000

    first commit

---
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+
+void print_diagsums(int *a, int size)
+{
+	int i, sum, sum2, n;
+
+	n = 0;
+	sum = 0;;
+	sum2 = 0;
+	for (i = 0; i < size; i++)
+	{
+		sum = sum + (a[(size * n) + i]);
+		sum2 = sum2 + (a[((size - 1) * (n + 1))]);
+		n++;
+	}
+	printf("%d, %d", sum, sum2);
+	putchar('\n');
+}
