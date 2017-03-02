a7e70e4e5df299870593e56a9fcf940686a051c5
kedpak <kedpak@yahoo.com>
2017-03-02 05:55:41 +0000

    first commit

---
+++ 0x07-recursion/7-is_palindrome.c
@@ -0,0 +1,56 @@
+#include "holberton.h"
+
+/**
+ * is_palindrome - checks if string is palindrome
+ * @s: string input
+ * Return: 1 or 0
+ */
+int is_palindrome(char *s)
+{
+	if (check(s, s[0], s[length(s) - 1]) == 1)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+		return (0);
+}
+
+/**
+ * check - checks characters that match
+ * @string: string input
+ * @start: start of string
+ * @end: end of string
+ * Return: result
+ */
+int check(char *string, char start, char end)
+{
+	if (string[start / 2] == string[end / 2])
+	{
+		return (check(string, start + 1, end - 1));
+	}
+	else if (string[(start / 2) + 1] == string[(end / 2) - 1])
+	{
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * length - length of string
+ * @c: character input
+ * Return: length
+ */
+int length(char *c)
+{
+	if (*c == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + length(c + 1));
+	}
+}
--- 0x07-recursion/holberton.h
+++ 0x07-recursion/holberton.h
@@ -8,3 +8,6 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int counter(int i, int k);
 int prime(int num, int i);
+int is_palindrome(char *s);
+int check(char *string, char start, char end);
+int length(char *c);
