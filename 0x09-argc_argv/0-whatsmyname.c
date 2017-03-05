4ac075b58f87a6a5c127da7476b1b881793cfad0
kedpak <kedpak@yahoo.com>
2017-03-05 02:46:12 +0000

    first commit

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function with arguments
+ * @argc: checks integer value of elements in array argv
+ * @argv: array of string inputs
+ * Return: number of elements
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (argc);
+}
---
+++ 0x09-argc_argv/README.md
@@ -0,0 +1,1 @@
+**Understanding the argc and argv arguments**
