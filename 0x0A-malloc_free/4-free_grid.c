7c3cf65479409c07236735c1b8bdd54ee863c411
kedpak <kedpak@yahoo.com>
2017-03-08 07:17:42 +0000

    betty issues

--- 0x0A-malloc_free/4-free_grid.c
+++ 0x0A-malloc_free/4-free_grid.c
@@ -2,9 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * free_grid - free grid
+ * @grid: grid
+ * @height: height
+ * Return: return void
+ */
 void free_grid(int **grid, int height)
 {
 	int i;
+
 	for (i = 0; i < height; i++)
 	{
 		free(grid[i]);
