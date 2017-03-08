ca87a8117cc520990ce8a1f7c6d506781ebcab81
kedpak <kedpak@yahoo.com>
2017-03-08 06:24:15 +0000

    fixed malloc

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -13,7 +13,7 @@ int **alloc_grid(int width, int height)
 	int i, j;
 	int **m;
 
-	m = malloc(sizeof(int *));
+	m = malloc(sizeof(int *) * width + height);
 	if (m == NULL || width <= 0 || height <= 0)
 	{
 		return (NULL);
