59a44043399a33fb0975991f43dba30d8104d166
kedpak <kedpak@yahoo.com>
2017-03-08 01:32:03 +0000

    added null issue

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -17,9 +17,13 @@ char *str_concat(char *s1, char *s2)
 	j = 0;
 	k = 0;
 	m = malloc(sizeof(char) * (_strlen(s1) + _strlen(s2) + 1));
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
-		return (NULL);
+		s1[0] = '\0';
+	}
+	if (s2 == NULL)
+	{
+		s2[0] = '\0';
 	}
 	while (s1[i] != '\0')
 	{
