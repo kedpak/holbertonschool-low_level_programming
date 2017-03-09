bfce906bdf77d10c6b6330d6c0fab1bd6d4ac1d8
kedpak <kedpak@yahoo.com>
2017-03-09 20:59:43 +0000

    first commit

---
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using pointer
+ * @b: input size
+ * Return: a
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *a;
+
+	a = malloc(b);
+	if (a == NULL)
+	{
+		exit(98);
+	}
+	return (a);
+}
