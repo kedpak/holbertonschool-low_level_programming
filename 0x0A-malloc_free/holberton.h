927dd00525f06235761f8ab217051e77b795598e
kedpak <kedpak@yahoo.com>
2017-03-10 02:20:19 +0000

    updated header file

--- 0x0A-malloc_free/holberton.h
+++ 0x0A-malloc_free/holberton.h
@@ -4,7 +4,12 @@ char *create_array(unsigned int size, char c);
 int _putchar(char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
-int _strlen(char *s);
+unsigned int _strlen(char *s);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+unsigned int total_len(int ac, char **av);
+unsigned sum_length(char **av);
+void copy_strings(char **av, char*s);
+void copy_str(char *f, char *t);
 #endif
