3221ada953c6ed6403f319a52352a4b23ac98528
kedpak <kedpak@yahoo.com>
2017-03-11 01:43:54 +0000

    changed condition

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -27,7 +27,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (ptr == NULL)
 	{
-		return (new);
+		return (malloc(new_size));
 	}
 	if (new_size == 0 && ptr != NULL)
 	{
