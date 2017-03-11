79373997bf69705b7a3f0358571d9f9e546f9310
kedpak <kedpak@yahoo.com>
2017-03-11 01:40:05 +0000

    first commit

---
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _realloc - reallocate memory from old block to new
+ * @ptr: pointer to old allocated memory
+ * @old_size: size of old allocation
+ * @new_size: size of new allocation
+ * Return: pointer to new memory
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new;
+	unsigned int i;
+	char *newcpy;
+	char *ptrcpy;
+
+	new = malloc(sizeof(int) * (old_size + new_size));
+	if (new_size > old_size)
+	{
+		new_size = old_size;
+	}
+	if (new_size == old_size)
+	{
+		return (new);
+	}
+	if (ptr == NULL)
+	{
+		return (new);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	newcpy = new;
+	ptrcpy = new;
+	for (i = 0; i < new_size; i++)
+	{
+		newcpy[i] = ptrcpy[i];
+	}
+	free(new);
+	return (new);
+}
