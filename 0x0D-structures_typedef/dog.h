ace08632ad835d1cd463259741f7b37d2ea2a1c7
kedpak <kedpak@yahoo.com>
2017-03-13 19:42:22 +0000

    first commit

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef dog
+#define dog
+#include <stdio.h>
+
+/**
+ * struct dog - describes information about dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+struct Dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
