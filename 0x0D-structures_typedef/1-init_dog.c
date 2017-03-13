0c716a52c209d82b76be33d50a1c6f2c5622980a
kedpak <kedpak@yahoo.com>
2017-03-13 21:17:15 +0000

    first commit

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * init_dot - initialize variable of type struct dog
+ * @d: struct dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
