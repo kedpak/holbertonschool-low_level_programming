0e935d0678c04498d6e32020db733b45b0dc35b1
kedpak <kedpak@yahoo.com>
2017-03-13 21:51:33 +0000

    added edge cases

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -9,6 +9,16 @@
  */
 void print_dog(struct dog *d)
 {
+	if (d == NULL)
+	{
+		return;
+	}
+	if (d->name == NULL || d->owner == NULL)
+	{
+		printf("Name: (nil)\n");
+		printf("Age: (nil)\n");
+		printf("Owner: (nil)\n");
+	}
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
