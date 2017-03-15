99f0d953ba9b5401f73fbf369640e9908918fdf8
kedpak <kedpak@yahoo.com>
2017-03-15 18:15:55 +0000

    first commit

---
+++ 0x0E-function_pointers/function_pointers.h
@@ -0,0 +1,5 @@
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+char *_strcpy(char *dest, char *src);
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
