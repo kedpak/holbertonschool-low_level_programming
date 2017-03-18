e5e7b1efb44f008b0093fdea7a2bf7f741c953dd
kedpak <kedpak@yahoo.com>
2017-03-18 00:09:44 +0000

    added null condition

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -17,14 +17,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%i", va_arg(ap, int));
-		if (i != n - 1)
+		if (i != n - 1 && separator != NULL)
 		{
 			printf("%s", separator);
 		}
-		else
-		{
-			printf("\n");
-		}
 	}
+	printf("\n");
 	va_end(ap);
 }
