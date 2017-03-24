32e5e3fc85f99ab12891140221605a9a37827daf
kedpak <kedpak@yahoo.com>
2017-03-24 01:49:29 +0000

    removed null condition

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -15,16 +15,13 @@ list_t *add_node(list_t **head, const char *str)
 	const char *temp = str;
 	struct list_s *new_node = (list_t *)malloc(sizeof(struct list_s));
 
-	if (head == NULL)
-	{
-		return (NULL);
-	}
 	new_node->str = strdup(temp);
 	new_node->len = strlen(temp);
 	new_node->next = *head;
 	if (head != NULL)
 	{
 		*head = new_node;
+		return (*head);
 	}
-	return (new_node);
+	return (NULL);
 }
