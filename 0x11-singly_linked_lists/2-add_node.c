4a7b305be597dc51ade56088c31fd379ea1c3720
kedpak <kedpak@yahoo.com>
2017-03-24 02:37:53 +0000

    implemented custom string length

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -4,6 +4,22 @@
 #include "lists.h"
 
 /**
+ * _strlen - return length of string
+ * @s: input string
+ * Return: return string length
+ */
+int _strlen(const char *s)
+{
+	int m;
+
+	m = 0;
+	while (s[m] != '\0')
+	{
+		m++;
+	}
+	return (m);
+}
+/**
  * add_node - add a new node at beginning of the list_t
  * @head: addresss of head
  * @str: input data
@@ -16,7 +32,7 @@ list_t *add_node(list_t **head, const char *str)
 	struct list_s *new_node = (list_t *)malloc(sizeof(struct list_s));
 
 	new_node->str = strdup(temp);
-	new_node->len = strlen(temp);
+	new_node->len = _strlen(temp);
 	new_node->next = *head;
 	if (head != NULL)
 	{
--- 0x11-singly_linked_lists/lists.h
+++ 0x11-singly_linked_lists/lists.h
@@ -15,6 +15,7 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+int _strlen(const char *s);
 size_t print_list(const list_t *h);
 int _putchar(char c);
 size_t list_len(const list_t *h);
