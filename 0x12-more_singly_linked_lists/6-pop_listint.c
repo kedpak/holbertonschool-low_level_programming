288fa92213e04801c983707fb0caa7ee7e5e9de4
kedpak <kedpak@yahoo.com>
2017-03-28 20:14:20 +0000

    first commit

---
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - remove first node fromt list
+ * @head: head node
+ * Return: data of node
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		temp = *head;
+		*head = temp->next;
+		free(temp);
+		return (temp->n);
+	}
+}
