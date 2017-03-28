75a66488412beb658feb68ed37a9cbc2073f320e
kedpak <kedpak@yahoo.com>
2017-03-28 20:46:33 +0000

    added data varaible

--- 0x12-more_singly_linked_lists/6-pop_listint.c
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -8,15 +8,17 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
+	unsigned int data;
 
 	if (head == NULL || *head == NULL)
 	{
 		return (0);
 	}
+	data = (*head)->n;
 	temp = (*head)->next;
 	free(*head);
 	*head = temp;
 
-	return (temp->n);
+	return (data);
 
 }
