c1e847c433342d9b6d3064efc73647053cefa488
kedpak <kedpak@yahoo.com>
2017-03-28 21:05:58 +0000

    fixed betty issues

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -19,7 +19,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	temp = head;
 	while (temp != NULL)
 	{
-       		if (i == index)
+		if (i == index)
 		{
 			return (temp);
 		}
