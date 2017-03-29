6fbb1bd619ee77627c38bcc73b95873313221aa2
kedpak <kedpak@yahoo.com>
2017-03-29 01:59:23 +0000

    added a idx = 0 condition, and if i is greater than idx

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,10 +19,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->next = NULL;
 	i = 0;
 
-/*	if (head == NULL || *head == NULL)
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (*head);
+	}
+	if (i > idx)
 	{
 		return (NULL);
-		} */
+	}
 	while (temp != NULL)
 	{
 		if (i == idx - 1)
