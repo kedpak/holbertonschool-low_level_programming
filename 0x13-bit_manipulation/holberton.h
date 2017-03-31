2cbe2b7cde5b72eb492f48f9a76659642ff3dfa3
kedpak <kedpak@yahoo.com>
2017-03-31 00:46:14 +0000

    updated header file

--- 0x13-bit_manipulation/holberton.h
+++ 0x13-bit_manipulation/holberton.h
@@ -1,8 +1,10 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-
+#include <stdlib.h>
 #include <stdio.h>
 int _strlen(const char *s);
 unsigned int binary_to_uint(const char *b);
 void rev_string(const char *s);
+void print_binary(unsigned long int n);
+int _putchar(char c);
 #endif
