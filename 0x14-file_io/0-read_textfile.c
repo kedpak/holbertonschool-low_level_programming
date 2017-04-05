bcbc48a18644d44e05ee32ad7f9bf7fc5b84887c
kedpak <kedpak@yahoo.com>
2017-04-05 04:29:14 +0000

    added conditions read and write functions

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -20,14 +20,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	r = read(fd, buf, letters);
-	written = write(STDOUT_FILENO, buf, r);
-	if (r != written)
+	if (r == -1)
 	{
 		free(buf);
-		return (0);
+		return (-1);
+	}
+	written = write(STDOUT_FILENO, buf, r);
+	if (written == -1)
+	{
+		free(buf);
+		return (-1);
 	}
 	close(fd);
-
 	free(buf);
 	return (written);
 }
