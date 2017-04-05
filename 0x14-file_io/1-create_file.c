5eb1ba419a8a4087ef2863663f62b5e313ed019b
kedpak <kedpak@yahoo.com>
2017-04-05 04:45:55 +0000

    added condition if w does not equal string length

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -24,33 +24,26 @@ int _strlen(char *s)
 int create_file(const char *filename, char *text_content)
 {
 	int fd, len, w;
-	char *buf;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (fd == -1)
-	{
-		close(fd);
-		return (-1);
-	}
 	len = _strlen(text_content);
-	buf = malloc(sizeof(char) * len);
+
 	if (text_content == NULL)
 	{
-		free(buf);
 		close(fd);
 		return (1);
 	}
 
 	w = write(fd, text_content, len);
-	if (w == -1)
+	if (w == -1 || w != len)
 	{
-		free(buf);
 		return (-1);
 	}
-	free(buf);
+
+	close(fd);
 	return (1);
 }
