d012bbbddbedccfbc04b04630759ab68c0a394f4
kedpak <kedpak@yahoo.com>
2017-04-05 06:41:30 +0000

    fixed NULL conditon

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -27,19 +27,21 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, len, w;
 
-	len = _strlen(text_content);
-	fd = open(filename, O_RDWR | O_APPEND);
 	if (filename == NULL)
 	{
-		close(fd);
 		return (-1);
 	}
+	fd = open(filename, O_RDWR | O_APPEND);
 	if (fd == -1)
 	{
 		close(fd);
 		return (-1);
 	}
-	w = write(fd, text_content, len);
+	if (text_content != NULL)
+	{
+		len = _strlen(text_content);
+		w = write(fd, text_content, len);
+	}
 	if (w == -1)
 	{
 		close(fd);
