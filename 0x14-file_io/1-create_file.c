03eaf08402e551a1033cc4fe38fc81e574e853ee
kedpak <kedpak@yahoo.com>
2017-04-05 06:29:29 +0000

    changed permissions for file

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -24,12 +24,13 @@ int _strlen(char *s)
 int create_file(const char *filename, char *text_content)
 {
 	int fd, len, w, closer;
+	mode_t mode = S_IWUSR | S_IRUSR;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
 	if (fd == -1)
 	{
 		return (-1);
@@ -38,7 +39,6 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		close(fd);
 		return (-1);
 	}
 
