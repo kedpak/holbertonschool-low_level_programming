79074fada9827d9bdda72bda9738005ef219d554
kedpak <kedpak@yahoo.com>
2017-04-06 20:40:22 +0000

    fixed betty line issues

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -1,9 +1,32 @@
+#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+
+/**
+ * write_error - prints out error for write
+ * @file: input file
+ * Return: void
+ */
+void write_error(const char *file)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+	exit(99);
+}
+
+/**
+ * read_error - prints out read error
+ * @file: input file
+ * Return: void
+ */
+void read_error(const char *file)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+	exit(98);
+}
 
 /**
  * main - copy contents of file to another
@@ -18,42 +41,34 @@ int main(int argc, char *argv[])
 
 	file1 = open(argv[1], O_RDONLY);
 	file2 = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
-
 	if (argc != 3)
 	{
-		printf("Usage: cp file_from file_to\n");
-		exit(97);
+		printf("Usage: cp file_from file_to\n"); exit(97);
 	}
 	f_from = read(file1, buf, 1024);
 	if (f_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE");
-		exit(98);
+		read_error(argv[1]);
 	}
 	while (f_from > 0)
 	{
 		f_to = write(file2, buf, f_from);
-		if (f_from == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE");
-			exit(99);
-		}
 		if (f_to == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE");
-			exit(99);
+			write_error(argv[2]);
 		}
 		f_from = read(f_from, buf, 1024);
 	}
 	if (close(f_from) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE");
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE"); exit(100);
 	}
 	if (close(f_to) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE");
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE"); exit(100);
 	}
 	return (0);
 }
+
+
+
