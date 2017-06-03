303c52ee019ffc6862b692632ab545a7c623db4e
kedpak <kedpak@yahoo.com>
2017-06-03 23:47:31 +0000

    first commit

---
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_t - creates a hash table
+ * @size: size of array which represents hash table
+ * Return: pointer to newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table;
+	unsigned long int i;
+
+	hash_table = malloc(sizeof(hash_table_t));
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+
+	for (i = 0; i < size; i++)
+	{
+		hash_table->array[i] = NULL;
+	}
+
+	hash_table->size = size;
+	return (hash_table);
+}
