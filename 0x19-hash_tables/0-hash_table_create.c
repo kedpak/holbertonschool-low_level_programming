2bca0e1e61a76a86630411ceb0ef3caa898f4bf2
kedpak <kedpak@yahoo.com>
2017-06-04 02:42:29 +0000

    fixed betty

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -12,7 +12,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hash_table = NULL;
 
-	if (size < 0)
+	if (size == 0)
 	{
 		return (NULL);
 	}
@@ -31,12 +31,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 
-	hash_table->size = size;
+
 	for (i = 0; i < size; i++)
 	{
 		hash_table->array[i] = NULL;
 	}
-
+	hash_table->size = size;
 
 	return (hash_table);
 }
