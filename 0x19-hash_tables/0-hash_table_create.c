5fae5e9c18cd527fd9ad4677b19a5fe057ec2ac2
kedpak <kedpak@yahoo.com>
2017-06-04 02:29:09 +0000

    changed position of malloc

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -16,13 +16,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 
 	hash_table = malloc(sizeof(hash_table_t));
-	hash_table->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (hash_table == NULL)
 	{
 		free(hash_table);
 		return (NULL);
 	}
+
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+
 	if (hash_table->array == NULL)
 	{
 		free(hash_table->array);
