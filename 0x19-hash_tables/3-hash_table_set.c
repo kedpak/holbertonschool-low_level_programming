29d88363c02d374a670b535142cb9b4cb3db502f
kedpak <kedpak@yahoo.com>
2017-06-04 23:23:40 +0000

    first commit

---
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds element to index of hash table
+ * @ht: pointer to hash table
+ * @key: key to be placed
+ * @value: value of key
+ * Return: 1 on success, 0 on failure
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *head;
+	unsigned long int index;
+
+	if (key == NULL)
+	{
+		return (0);
+	}
+
+	index = key_index((unsigned const char*)key, ht->size);
+	head = malloc(sizeof(hash_node_t *));
+
+	ht->array[index] = head;
+	head->key = (char *)key;
+	head->value = (char *)value;
+	head->next = NULL;
+
+	return (1);
+}
