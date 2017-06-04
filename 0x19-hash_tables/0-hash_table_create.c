cbbd6d096440bf65f47b5088ff4ddf3c3da34f2b
kedpak <kedpak@yahoo.com>
2017-06-04 02:39:31 +0000

    fixed betty issue

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_t - creates a hash table
+ * hash_table_create - creates a hash table
  * @size: size of array which represents hash table
  * Return: pointer to newly created hash table
  */
@@ -12,7 +12,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hash_table = NULL;
 
-	if (size == 0)
+	if (size < 0)
 	{
 		return (NULL);
 	}
@@ -21,7 +21,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (hash_table == NULL)
 	{
-		free(hash_table);
 		return (NULL);
 	}
 
@@ -29,15 +28,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (hash_table->array == NULL)
 	{
-		free(hash_table->array);
 		return (NULL);
 	}
 
+	hash_table->size = size;
 	for (i = 0; i < size; i++)
 	{
 		hash_table->array[i] = NULL;
 	}
 
-	hash_table->size = size;
+
 	return (hash_table);
 }
