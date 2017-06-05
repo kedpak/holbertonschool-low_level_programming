934cbe0b8e1e772a001450a48a39e9e3b0dae66a
kedpak <kedpak@yahoo.com>
2017-06-05 01:00:56 +0000

    commented out collsion condition

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -21,11 +21,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	index = key_index((unsigned const char*)key, ht->size);
 	head = malloc(sizeof(hash_node_t *));
 
-	if (ht->array[index] != NULL)
+/*	if (ht->array[index] != NULL)
 	{
 		_add_node(head, key, value);
 		return (1);
-	}
+		}*/
 	ht->array[index] = head;
 	head->key = (char *)key;
 	head->value = (char *)value;
