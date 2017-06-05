361ab68a8ae28d1520d138f3fd3a8aa96d211027
kedpak <kedpak@yahoo.com>
2017-06-05 00:48:48 +0000

    added collision condition

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -12,14 +12,20 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *head;
 	unsigned long int index;
 
-	if (key == NULL)
+	if (ht == NULL || key == NULL || value == NULL)
 	{
+		free(ht);
 		return (0);
 	}
 
 	index = key_index((unsigned const char*)key, ht->size);
 	head = malloc(sizeof(hash_node_t *));
 
+	if (ht->array[index] != NULL)
+	{
+		_add_node(head, key, value);
+		return (1);
+	}
 	ht->array[index] = head;
 	head->key = (char *)key;
 	head->value = (char *)value;
