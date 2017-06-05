42171bdfc153b589c20014420fd78102bd548b68
kedpak <kedpak@yahoo.com>
2017-06-05 09:32:23 +0000

    added new function to handle key/value assignment

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -9,7 +9,7 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *head, *new_node;
+	hash_node_t *temp, *new_node;
 	unsigned long int index;
 
 	if (ht == NULL || ht->array == NULL || ht->size == 0 || key == NULL)
@@ -18,35 +18,66 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 
 	index = key_index((unsigned const char*)key, ht->size);
-	head = malloc(sizeof(hash_node_t *));
 
 	ht->array[index] = NULL;
+	temp = ht->array[index];
+	new_node = ht->array[index];
 	if (ht->array[index] != NULL)
 	{
-		while (head != NULL)
+		while (temp != NULL)
 		{
-			if (head && head->key != NULL && strcmp(key, strdup(head->key)) == 0)
+			if (temp && temp->key != NULL && strcmp(key, strdup(temp->key)) == 0)
 			{
-				free(head->value);
-				head->value = strdup(value); return (1);
+				free(temp->value);
+				temp->value = strdup(value);
+				return (1);
 			}
 		}
-		new_node = malloc(sizeof(hash_node_t *));
-		new_node->key = strdup(key);
-		new_node->value = strdup(value);
-		new_node->next = head;
+		/* this is where all the value sets go */
+		new_node = set_values(key, value);
+		new_node->next = ht->array[index];
 		ht->array[index] = new_node;
 		if (new_node->key == NULL || new_node->value == NULL)
 		{
-			free(new_node); return (0);
+			free(new_node);
+			return (0);
 		}
 	}
 	else
 	{
-		ht->array[index] = head;
-		head->key = strdup(key);
-		head->value = strdup(value);
-		head->next = NULL; return (1);
+		new_node = set_values(key, value);
+		ht->array[index] = new_node;
 	}
 	return (1);
+}
+
+
+/**
+ * set_values - sets the key and values of the node
+ * @key: key to be placed into node
+ * @value: value of key
+ * Return: returns the new node
+ */
+hash_node_t *set_values(const char *key, const char *value)
+{
+	hash_node_t *new_node;
+
+	new_node = malloc(sizeof(hash_node_t *));
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		free(new_node->key);
+		return (NULL);
+	}
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node);
+		free(new_node->value);
+		return (NULL);
+	}
+	new_node->next = NULL;
+	return (new_node);
+
 }
