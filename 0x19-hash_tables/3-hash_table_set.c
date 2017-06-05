1bd6aa39977bea8ccbf34f9b8f06edd4393eae77
kedpak <kedpak@yahoo.com>
2017-06-05 06:53:08 +0000

    testing3

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -27,18 +27,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		head->key = strdup(key);
 		head->value = strdup(value);
 		head->next = head;
-		while (head->next != NULL)
-		{
-			if (head != NULL && head->key != NULL && strcmp(key, head->key)\
-			    == 0)
-			{
-				free(head->value);
-				head->value = strdup(value);
-				return (1);
-			}
-			head = head->next;
-		}
-
 	}
 	else
 	{
