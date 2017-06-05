230931bdcfda5e9e2bd48d88c9270994e3d1a465
kedpak <kedpak@yahoo.com>
2017-06-05 03:35:55 +0000

    removed condition

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -21,11 +21,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	head = malloc(sizeof(hash_node_t *));
 	new_node = malloc(sizeof(hash_node_t *));
 
-	if (ht->array[index] != NULL && strcmp(key, new_node->key) == 0)
-	{
-		free (new_node->value);
-		new_node->value = strdup(value);
-	} 
 	if (ht->array[index] != NULL)
 	{
 		new_node->key = (char *)key;
