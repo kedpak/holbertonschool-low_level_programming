dfdb6ad9f04f9389a66d15012cf3fe7a871d3742
kedpak <kedpak@yahoo.com>
2017-06-05 05:47:26 +0000

    added condition for duplicate key

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -23,7 +23,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (head != NULL && head->key != NULL && strcmp(key, head->key) == 0)
 	{
-		free(new_node->value);
+		free(head->value);
 		head->value = strdup(value);
 		return (1);
 	}
