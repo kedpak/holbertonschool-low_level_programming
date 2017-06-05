4557175f9df36374d8b1ff370c0c9b79ee4ccb92
kedpak <kedpak@yahoo.com>
2017-06-05 21:40:57 +0000

    added NULL conditions and fixed typo

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -11,7 +11,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *k_pair;
 	unsigned long int index;
 
-	index = key_index((unsigned const char*)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 	k_pair = ht->array[index];
 
 	if (k_pair == NULL || k_pair->value == NULL)
@@ -19,6 +19,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 	}
 	if (strcmp(strdup(key), k_pair->key) != 0)
+	{
+		return (NULL);
+	}
+	if (ht == NULL || key == NULL)
 	{
 		return (NULL);
 	}
