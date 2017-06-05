0f9a017f1f526d5bab1fa45a63de744414ddf29f
kedpak <kedpak@yahoo.com>
2017-06-05 02:58:40 +0000

    added more 0 conditions

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -12,14 +12,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *head, *new_node;
 	unsigned long int index;
 
-	if (ht == NULL || key == NULL || value == NULL || ht->array == NULL)
+	if (ht == NULL || ht->array == NULL || ht->size == 0 || key == NULL)
 	{
 		return (0);
 	}
 
 	index = key_index((unsigned const char*)key, ht->size);
 	head = malloc(sizeof(hash_node_t *));
-
 
 	if (ht->array[index] != NULL)
 	{
