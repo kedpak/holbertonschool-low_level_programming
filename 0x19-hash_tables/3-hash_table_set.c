1cd68ac66817078a3d7329ce9dbfdb3ef33c03a3
kedpak <kedpak@yahoo.com>
2017-06-05 22:29:32 +0000

    re-added duplicate key condition

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -9,41 +9,44 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node;
-	unsigned long int index;
+	hash_node_t *new_node, *temp; unsigned long int index;
 
 	if (ht == NULL || ht->array == NULL || ht->size == 0 || key == NULL)
-	{
 		return (0);
-	}
 	if (value == 0 || strlen(key) == 0)
-	{
 		return (0);
-	}
-
+	/* grabs index of array with key_index function */
 	index = key_index((unsigned const char*)key, ht->size);
-
+	/* assigns new node and temp to struct at array[index] */
 	new_node = ht->array[index];
-	ht->array[index] = NULL;
-	if (ht->array[index] == NULL)
+	temp = ht->array[index];
+	/* beginning of linked list at specified index */
+	if (new_node != NULL)
 	{
-		new_node = set_values(key, value);
+		new_node = set_values(key, value);  /* malloc space for new node */
 		if (new_node == NULL)
-		{
 			return (0);
-		}
 		ht->array[index] = new_node;
 	}
 	else
 	{
+		/* checks if there is a duplicate key by looping through the temp node */
+		while (temp != NULL)
+		{
+			/* if matching key, then override the previous value with new value */
+			if (temp && temp->key && strcmp(key, strdup(temp->key)) == 0)
+			{
+				free(temp->value);
+				temp->value = strdup(value); return (1);
+			}
+			temp = temp->next;
+		}
+		/* sets the new node of the list and place at beginning of list */
 		new_node = set_values(key, value);
 		if (new_node == NULL)
-		{
 			return (0);
-		}
 		new_node->next = new_node;
 		ht->array[index] = new_node;
-
 	}
 	return (1);
 }
@@ -53,6 +56,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
  * set_values - creates new node and sets the key and values of the node
  * @key: key to be placed into node
  * @value: value of key
+ * function mallocs space for new node of linked list and sets values of node
  * Return: returns the new node
  */
 hash_node_t *set_values(const char *key, const char *value)
@@ -60,13 +64,19 @@ hash_node_t *set_values(const char *key, const char *value)
 	hash_node_t *new_node;
 
 	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
 	new_node->key = strdup(key);
+	/* if key of new node is NULL, then free hte new node */
 	if (new_node->key == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
 	new_node->value = strdup(value);
+	/* if value is NULL, then free all previous malloced values */
 	if (new_node->value == NULL)
 	{
 		free(new_node);
