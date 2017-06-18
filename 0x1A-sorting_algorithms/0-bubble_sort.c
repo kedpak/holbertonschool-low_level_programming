1d624c17938a51c00de34d62016a5a7005a4288f
kedpak <kedpak@yahoo.com>
2017-06-18 01:47:18 +0000

    first commit

---
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts array of integers from smallest to argest
+ * @array: pointer to array to be sorted
+ * @size: size of the array/number of element inside array
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp;
+
+	i = 0;
+	j = 0;
+	while (j < size - 1)
+	{
+		i = 0;
+		while (i < size - 1)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				print_array(array, size);
+			}
+			i++;
+		}
+		j++;
+	}
+
+}
---
+++ 0x1A-sorting_algorithms/README.md
@@ -0,0 +1,12 @@
+# Sorting Algorithms and Big 0 Notation
+
+## Synopsis
+Practice problems for sorting algorithms in C.
+
+This project utilizes four different sorting algorithms (bubble, insertion, selection, quick sort) and their Big 0 runtimes.
+
+## Description
+A sorting algorithm is an algorithm that sorts elements of a list to a speicific type of order. 
+
+Big-O represents an equation that returns the runtime of an algorithm. 
+
