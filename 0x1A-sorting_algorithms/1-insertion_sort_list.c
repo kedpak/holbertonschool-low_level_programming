4a36ca86c8dc00860d270e45d13cdd75dd881f47
kedpak <kedpak@yahoo.com>
2017-06-22 07:32:44 +0000

    first commit

---
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts linked list from lowest data int to largest
+ * @list: list to sort
+ * Return: void
+ * list is traversed forward and trav in reverse if int is lower than prev int
+ */
+void insertion_sort_list(listint_t **list)
+{
+
+	listint_t *current, *temp, *temp2;
+
+	current = *list;
+	if (list == NULL || *list == NULL)
+	{
+		return;
+	}
+	while (current != NULL)
+	{
+		while (current->prev != NULL && (current->prev)->n > current->n)
+		{
+			temp = current;
+			temp2 = current->prev;
+			temp2->next = temp->next;
+			if (temp2->next != NULL)
+			{
+				temp2->next->prev = temp2;
+			}
+			temp->prev = temp2->prev;
+			if (temp->prev != NULL)
+			{
+				temp->prev->next = temp;
+			}
+			else
+			{
+				*list = temp;
+			}
+			temp2->prev = temp;
+			temp->next = temp2;
+			print_list(*list);
+
+			current = temp2->prev;
+		}
+		current = current->next;
+	}
+}
