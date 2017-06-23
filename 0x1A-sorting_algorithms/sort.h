08991674f94f206460334cbc4630d61d5d31fa5b
kedpak <kedpak@yahoo.com>
2017-06-23 03:52:00 +0000

    first commit

--- 0x1A-sorting_algorithms/sort.h
+++ 0x1A-sorting_algorithms/sort.h
@@ -29,4 +29,5 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 int partition (int *array, int start, int end, size_t size);
 void _quick(int *array, int start, int end, size_t size);
+void cocktail_sort_list(listint_t **list);
 #endif /* SORT_H */
