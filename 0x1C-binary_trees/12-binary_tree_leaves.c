c507c63c3e76009c37d130c81ee3c44dab809aad
kedpak <kedpak@yahoo.com>
2017-07-26 21:15:36 +0000

    first commit

---
+++ 0x1C-binary_trees/12-binary_tree_leaves.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - find the number of leaves inside of a tree
+ * @tree: tree to count the nodes
+ * Return: size_t value of the number of nodes
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+		
+	}
+	else
+	{
+		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	}
+
+	return (0);
+}
