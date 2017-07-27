a40b07f10a7c141c712bfc3a0be2a38752d86a26
kedpak <kedpak@yahoo.com>
2017-07-27 21:26:28 +0000

    updated betty checks

--- 0x1C-binary_trees/2-binary_tree_insert_right.c
+++ 0x1C-binary_trees/2-binary_tree_insert_right.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_insert_right - insert node at right subtree of tree
+ * @parent: pointer to parent node of current node
+ * @value: integer value held inside node
+ * Return: node to be inserted
+ */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
@@ -18,18 +24,19 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (node);
 
 	}
-	else {
+	else
+	{
 		/* creates node with values but swaps parent.left and node.left */
 		node->parent = parent;
 		node->left = NULL;
-                node->right = NULL;
-                node->n = value;
+		node->right = NULL;
+		node->n = value;
 
 		node->right = parent->right;
-                parent->right = node;
-                node->right->parent = node;
+		parent->right = node;
+		node->right->parent = node;
 		return (node);
-        }
+	}
 
 	return (0);
 }
