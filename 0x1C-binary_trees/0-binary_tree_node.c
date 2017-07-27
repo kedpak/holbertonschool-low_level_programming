d03de198036a309a05bc0626852416c872538bd1
kedpak <kedpak@yahoo.com>
2017-07-27 21:23:33 +0000

    added null condition

--- 0x1C-binary_trees/0-binary_tree_node.c
+++ 0x1C-binary_trees/0-binary_tree_node.c
@@ -13,6 +13,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	node = malloc(sizeof(binary_tree_t));
 
+	if (node == NULL)
+	{
+		return (NULL);
+	}
 	node->parent = parent;
 	node->left = NULL;
 	node->right = NULL;
