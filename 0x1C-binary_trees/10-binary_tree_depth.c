4291280b8f3b28efa8d7eb7fca427cc5e6b862ff
kedpak <kedpak@yahoo.com>
2017-07-27 21:49:02 +0000

    updated betty checks

--- 0x1C-binary_trees/10-binary_tree_depth.c
+++ 0x1C-binary_trees/10-binary_tree_depth.c
@@ -9,11 +9,10 @@ size_t binary_tree_depth(const binary_tree_t *node)
 {
 	size_t depth;
 
-
 	if (node == NULL)
-        {
-                return (0);
-        }
+	{
+		return (0);
+	}
 
 	depth = 0;
 	while (node != NULL && node->parent != NULL)
