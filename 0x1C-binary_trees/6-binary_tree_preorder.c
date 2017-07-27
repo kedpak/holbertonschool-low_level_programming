d47e326874bf4b02bd7b8c13fd0d07e4b7f9a364
kedpak <kedpak@yahoo.com>
2017-07-27 21:39:48 +0000

    updated betty check

--- 0x1C-binary_trees/6-binary_tree_preorder.c
+++ 0x1C-binary_trees/6-binary_tree_preorder.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_preorder - traverse tree with preorder traversal
+ * @tree: tree to be traversed
+ * @func: function pointer to function
+ * Return: void function
+ */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL)
