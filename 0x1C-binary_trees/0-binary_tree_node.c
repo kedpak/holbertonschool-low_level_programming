61fda5d221d10a912a4890597150e8445f264fc9
kedpak <kedpak@yahoo.com>
2017-07-27 21:18:33 +0000

    fixed betty checks

--- 0x1C-binary_trees/0-binary_tree_node.c
+++ 0x1C-binary_trees/0-binary_tree_node.c
@@ -1,11 +1,18 @@
 #include "binary_trees.h"
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
+/**
+ * binary_tree_node - creates a node for a binary tree
+ * @parent: pointer to the parent of a node
+ * @value: integer value held inside node
+ * Return: node to be placed inside tree
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
 
 	binary_tree_t *node;
-	
+
 	node = malloc(sizeof(binary_tree_t));
-	
+
 	node->parent = parent;
 	node->left = NULL;
 	node->right = NULL;
