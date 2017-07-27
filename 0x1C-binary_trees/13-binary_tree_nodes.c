1b357750fcf45060a23ded27125c91131c863da2
kedpak <kedpak@yahoo.com>
2017-07-27 21:53:13 +0000

    updated betty checks

--- 0x1C-binary_trees/13-binary_tree_nodes.c
+++ 0x1C-binary_trees/13-binary_tree_nodes.c
@@ -1,20 +1,26 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes - return the count for number of nodes that have atleast one child
+ * binary_tree_nodes - return the count for number nodes with atleast one child
  * @tree: tree to count nodes with children
  * Return: size_t type number of nodes with children
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+
+	size_t left, right;
+
 	if (tree == NULL)
 	{
 		return (0);
 	}
 
+	left = binary_tree_nodes(tree->left);
+	right = binary_tree_nodes(tree->right);
+
 	if (tree->left != NULL || tree->right != NULL)
 	{
-		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
+		return (left + right + 1);
 	}
 
 	return (0);
