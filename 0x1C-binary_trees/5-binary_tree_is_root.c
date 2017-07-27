859c8e2c6226457505ab54ca3941ca468ff880af
kedpak <kedpak@yahoo.com>
2017-07-27 21:35:21 +0000

    updated betty checks

--- 0x1C-binary_trees/5-binary_tree_is_root.c
+++ 0x1C-binary_trees/5-binary_tree_is_root.c
@@ -1,5 +1,10 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_is_root - checks if node is a root
+ * @node: node to be checked if root or not
+ * Return: returns 1 if root, 0 if node is not root
+ */
 int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL)
