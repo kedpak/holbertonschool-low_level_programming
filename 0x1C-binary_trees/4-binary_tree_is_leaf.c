f3decf15d01486ebc8ef16f846055cece38501dc
kedpak <kedpak@yahoo.com>
2017-07-27 21:33:58 +0000

    updated betty checks

--- 0x1C-binary_trees/4-binary_tree_is_leaf.c
+++ 0x1C-binary_trees/4-binary_tree_is_leaf.c
@@ -1,5 +1,10 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_is_leaf - check if node is a leaf
+ * @node: node to be checked if leaf
+ * Return: returns 1 if node is leaf, 0 if node is not leaf
+ */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
