15f4e4752ec080996a25cb3fbd44d744c0e1db22
kedpak <kedpak@yahoo.com>
2017-07-28 19:49:25 +0000

    updated functions

--- 0x1C-binary_trees/binary_trees.h
+++ 0x1C-binary_trees/binary_trees.h
@@ -43,5 +43,6 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 size_t _pow_recursion(size_t x, size_t y);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 
 #endif /* BINARY TREES */
