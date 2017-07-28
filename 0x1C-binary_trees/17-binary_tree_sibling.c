0d7bf7e318696f702bb9a4f12a1fbf81a18317de
kedpak <kedpak@yahoo.com>
2017-07-28 00:52:34 +0000

    first commit

---
+++ 0x1C-binary_trees/17-binary_tree_sibling.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - return the value of a sibling of node
+ * @node: node to find sibling of
+ * Return: sibling of the node
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+
+	if (node->parent == NULL)
+	{
+		return (NULL);
+	}
+
+	if (node == node->parent->right)
+	{
+		if (node->parent->left != NULL)
+		{
+			return (node->parent->left);
+		}
+		return (NULL);
+	}
+	if (node == node->parent->left)
+	{
+		if (node->parent->right != NULL)
+		{
+			return (node->parent->right);
+		}
+		return (NULL);
+	}
+
+	return (NULL);
+}
