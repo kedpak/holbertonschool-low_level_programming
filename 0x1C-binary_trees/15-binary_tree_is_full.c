da70b270149d9c5d1bc50b40ba87500ee2e16f6d
kedpak <kedpak@yahoo.com>
2017-07-28 01:37:55 +0000

    fixed main file to test

--- 0x1C-binary_trees/15-binary_tree_is_full.c
+++ 0x1C-binary_trees/15-binary_tree_is_full.c
@@ -7,26 +7,18 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-
 	if (tree == NULL)
 	{
 		return (0);
 	}
 
-
-	if (tree->left != NULL && tree->left != NULL)
+	if (tree->left == NULL && tree->right == NULL)
 	{
-		if (tree->left->left == NULL && tree->right->right == NULL)
-		{
-			return (1);
-		}
-		else
-		{
-
-			return (0);
-		}
+		return (1);
 	}
-	binary_tree_is_full(tree->left);
-	binary_tree_is_full(tree->right);
+	if (tree->left != NULL && tree->right != NULL)
+	{
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	}
 	return (0);
 }
