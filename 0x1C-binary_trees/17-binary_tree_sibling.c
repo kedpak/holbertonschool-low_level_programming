1e50467019e71ef082dc3cefe6134d97473bd7cf
kedpak <kedpak@yahoo.com>
2017-07-28 03:09:51 +0000

    changed null conditions

--- 0x1C-binary_trees/17-binary_tree_sibling.c
+++ 0x1C-binary_trees/17-binary_tree_sibling.c
@@ -8,16 +8,16 @@
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 
-	if (node->parent == NULL || node == NULL)
+	if (!node->parent || !node)
 	{
 		return (NULL);
 	}
 
-	if (node->parent->left != NULL && node->parent->left != node)
+	if (node->parent->left && node->parent->left != node)
 	{
 		return (node->parent->left);
 	}
-	if (node->parent->right != NULL && node->parent->right != node)
+	if (node->parent->right && node->parent->right != node)
 	{
 		return (node->parent->right);
 	}
